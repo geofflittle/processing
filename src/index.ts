9b5783d5f8d8f6e8ab114d49fb6ede1607ed7f5c
Geoff Little <g.little712@gmail.com>
2019-02-19 10:11:14 -0600

    I dont know how, but this is cool

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as P5 from "p5";
 import "./assets/styles/main.scss";
 import {MathUtils} from "./math-utils";
-import {Dictionary} from "lodash";
 
 const p5: P5 = new P5(() => {});
 
@@ -15,9 +14,10 @@ const SKETCH_HEIGHT_BUFFER: number = 10;
 const ROTATION_DEGREES_X_COEFF: number = 15;
 const ROTATION_DEGREES_Y_COEFF: number = 20;
 const TOTAL_DEGREES: number = 360;
-const HUE_RANGE: number = 360;
-const NOISE_FRAME_COUNT_SPEED: number = .25;
+const HUE_NOISE_SPEED: number = 0.075;
 const NOISE_SIGMOID: (x: number) => number = MathUtils.getSigmoid(1, 10, 0.5);
+const X_SHIFT: number = 10;
+const Y_SHIFT: number = 50;
 
 let buffer: any;
 
@@ -36,22 +36,25 @@ function getRotationDegrees(i: number, j: number) {
     ROTATION_DEGREES_Y_COEFF * j % TOTAL_DEGREES;
 }
 
-function getHue(i: number, j: number) {
-  const noise: number = p5.noise(p5.map(i, 0, NUM_COLS, 0, 1), p5.map(j, 0, NUM_ROWS, 0, 1), p5.frameCount * .05);
-  const noise2: number = NOISE_SIGMOID(noise);
-  const range: number = 360;
-  return range - p5.abs(range - (noise2 * (range * 2)));
-  // return 2 * range * noise2 % range;
+function getHue(i: number, j: number, rotationDegrees: number) {
+  // Map the i's and j's so that they're "closer", from the perspective of the noise function
+  const mappedI: number = p5.map(i, 0, NUM_COLS, 0, 1);
+  const mappedJ: number = p5.map(j, 0, NUM_ROWS, 0, 1);
+  const mappedDegrees: number = p5.map(rotationDegrees, 0, 360, 0, 1);
+  // const noise: number = p5.noise(mappedI, mappedJ, mappedDegrees);
+  // Transform the noise function so it favors the middle less
+  // const noise2: number = NOISE_SIGMOID(noise);
+  return p5.map(mappedJ + mappedI + mappedDegrees, 0, 3, 0, 120);
 }
 
 function drawCircle(xCenter: number, yCenter: number, i: number, j: number) {
   const rotationDegrees: number = getRotationDegrees(i, j);
-  const xDelta: number = 0; // 50 * p5.cos(rotationDegrees);
-  const yDelta: number = 0; // 50 * p5.sin(rotationDegrees);
-  const hue: number = getHue(i, j);
+  const xShift: number = X_SHIFT * p5.cos(rotationDegrees);
+  const yShift: number = Y_SHIFT * p5.sin(rotationDegrees);
+  const hue: number = getHue(i, j, rotationDegrees);
 
-  buffer.fill(hue, 360, 360);
-  buffer.ellipse((i + 1) * xCenter + xDelta - 100, (j + 1) * yCenter + yDelta - 100, CIRCLE_WIDTH, CIRCLE_HEIGHT);
+  buffer.fill(hue * (p5.random([0, 1])), 360, 360);
+  buffer.ellipse((i + 1) * xCenter + xShift, (j + 1) * yCenter + yShift, CIRCLE_WIDTH, CIRCLE_HEIGHT);
 }
 
 p5.setup = () => {
@@ -59,7 +62,6 @@ p5.setup = () => {
   canvas.parent("content");
   buffer = p5.createGraphics(getSketchWidth(), getSketchHeight());
 
-  // p5.frameRate(30);
   buffer.colorMode(p5.HSB, 360);
   p5.colorMode(p5.HSB, 360);
   p5.ellipseMode(p5.CENTER);
@@ -68,32 +70,19 @@ p5.setup = () => {
 };
 
 p5.draw = () => {
-  p5.background(0);
-  const nSamples: number = 175000;
-  // const counts: Dictionary<number> = {};
-  Object.entries([...Array(nSamples).keys()]
-    .map((i: number) => p5.noise(i))
-    .map((x: number) => NOISE_SIGMOID(x))
-    .map((x: number) => p5.map(x, 0, 1, 0, getSketchWidth()))
-    .map((x: number) => Math.round(x))
-    .reduce((countsAccum: Dictionary<number>, currentValue) => {
-      countsAccum[currentValue] = countsAccum[currentValue] + 1 || 1;
-      return countsAccum;
-    }, {} as Dictionary<number>))
-    .forEach((entry: [string, number]) => {
-      console.log(entry);
-      const x: number = parseInt(entry[0]);
-      p5.line(x, getSketchHeight(), x, getSketchHeight() - entry[1]);
+  buffer.background(0, 0, 0, 45);
+  p5.rectMode(p5.CORNERS);
+  p5.strokeWeight(1);
+  p5.noFill();
+  p5.rect(0, 0, p5.width, p5.height);
+  const xCenter: number = (p5.windowWidth + 200) / (NUM_COLS + 1);
+  const yCenter: number = (p5.windowHeight + 200) / (NUM_ROWS + 1);
+  [...Array(NUM_COLS).keys()].forEach((i: number) => {
+    [...Array(NUM_ROWS).keys()].forEach((j: number) => {
+      drawCircle(xCenter, yCenter, i, j);
     });
-  // buffer.background(0, 0, 0);
-  // const xCenter: number = (p5.windowWidth + 200) / (NUM_COLS + 1);
-  // const yCenter: number = (p5.windowHeight + 200) / (NUM_ROWS + 1);
-  // [...Array(NUM_COLS).keys()].forEach((i: number) => {
-  //   [...Array(NUM_ROWS).keys()].forEach((j: number) => {
-  //     drawCircle(xCenter, yCenter, i, j);
-  //   });
-  // });
-  // p5.image(buffer, 0, 0);
+  });
+  p5.image(buffer, 0, 0);
 };
 
 p5.windowResized = () => {
